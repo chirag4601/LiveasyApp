9da7edf0fc82267db9dd6b81568b3e8c9e1493bc
chirag4601 <chirag4601@gmail.com>
2021-07-30 04:50:54 +0530

    emptied main

--- android/app/proguard-rules.pro
+++
@@ -1,1 +0,0 @@
--keep class com.liveasy.liveasy.BuildConfig { *; }
--- lib/controller/CompletedDateController.dart
+++
@@ -1,9 +0,0 @@
-import 'package:get/get.dart';
-
-class CompletedDateController extends GetxController {
-  RxString completedDate = "".obs;
-
-  void updateCompletedDateController(String newValue) {
-    completedDate.value = newValue;
-  }
-}
